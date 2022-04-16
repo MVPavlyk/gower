fe7a81f01ee920438cd230607faf836877bbb63a
MVPavlyk <grandbityyy67@gmail.com>
2022-04-16 14:03:42 +0300

    16.04 14:03

---
+++ src/Pages/RestaurantListPage/RestaurantListPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {RestaurantBlock} from '../../Components';
+import css from './RestaurantPageList.module.css'
+
+const RestaurantListPage = () => {
+    const storrage = [
+        {
+            id: 1,
+            name: 'Кафе-Бар “У Марти”',
+            address: 'Вул. Степана Бандери 2',
+            time: '09:00 - 21:00',
+            img: './img/u_marty.png'
+        },
+        {
+            id: 2,
+            name: 'Ресторан  “Панорама”',
+            address: 'Вул. Степана Бандери 2',
+            time: '09:00 - 21:00',
+            img: './img/panorama.png'
+        }
+    ]
+
+
+    return (
+        <div className={css.restaurant__wrap}>
+            {
+                storrage.map(restaurant => <RestaurantBlock key={restaurant.id} restaurant={restaurant}/>)
+            }
+        </div>
+    );
+};
+
+export {RestaurantListPage};
