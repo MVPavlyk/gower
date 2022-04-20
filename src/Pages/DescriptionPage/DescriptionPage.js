af7bc978c1a965bd4324c9fa2460ab55abe229c1
nadiiakhasyshyn <nadiia.khasyshyn.pm.2020@lpnu.ua>
2022-04-21 00:44:46 +0300

    0.44

--- src/Pages/DescriptionPage/DescriptionPage.js
+++ src/Pages/DescriptionPage/DescriptionPage.js
@@ -7,12 +7,39 @@ const DescriptionPage = () => {
 
     return (
         <div className={css.desc_page}>
+            <div className={css.desc_page1}>
             {oneRestaurant.desc_p_1}
             <img
                 src={`https://drive.google.com/uc?export=view&id=${oneRestaurant.desc_p_1_img}`}
                 alt={oneRestaurant.name}
             />
+            </div>
+            <div className={css.desc_page1}>
+
+            <img
+                src={`https://drive.google.com/uc?export=view&id=${oneRestaurant.desc_p_2_img}`}
+                alt={oneRestaurant.name}
+            />
+                {oneRestaurant.desc_p_2}
+            </div>
+            <div className={css.desc_page1}>
+            {oneRestaurant.desc_p_3}
+            <img
+                src={`https://drive.google.com/uc?export=view&id=${oneRestaurant.desc_p_3_img}`}
+                alt={oneRestaurant.name}
+            />
+            </div>
+            <div className={css.desc_page1}>
+
+            <img
+                src={`https://drive.google.com/uc?export=view&id=${oneRestaurant.desc_p_4_img}`}
+                alt={oneRestaurant.name}
+            />
+                {oneRestaurant.desc_p_4}
+            </div>
+
         </div>
+
     );
 };
 
