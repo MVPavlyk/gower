1c522ac1db8ef935d17ae1b01c41c711dfee2e83
Max2002-dbz <maks.kuk20022@gmail.com>
2022-05-20 00:16:06 +0300

    Батон

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,11 +8,18 @@ import sort_dark from '../../img/sort_dark.svg';
 import filter from '../../img/filter.svg';
 import filter_dark from '../../img/filter_dark.svg';
 
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {switchTheme} from '../../store/slices/theme.slice';
 
 const Navbar = () => {
 
     const {dark} = useSelector(state => state['themeReducers']);
+
+    const dispatch = useDispatch();
+
+    const switchT = () => {
+        dispatch(switchTheme());
+    };
 
     return (
         <div className={dark ? css.main_navbar_dark : css.main_navbar}>
@@ -33,6 +40,13 @@ const Navbar = () => {
                         <div>
                             <button className={dark ? css.nav_link_dark : css.nav_link}>
                                 <img src={dark ? filter_dark : filter} alt="filter"/> Filter
+                            </button>
+                        </div>
+                        <div>
+                            <button onClick={() => switchT()} className={dark ? css.dark_switch_btn : css.switch_btn}>
+                                <div className={dark ? css.dark_switch_btn_ball : css.switch_btn_ball}>
+
+                                </div>
                             </button>
                         </div>
                     </div>
