c8ac528b4b72b1648f3550fa9f0b14b88603c27f
MVPavlyk <grandbityyy67@gmail.com>
2022-05-20 00:22:58 +0300

    login

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -24,15 +24,6 @@ const Header = () => {
         setShow(false);
     }, [pathname]);
 
-
-    const {dark} = useSelector(state => state['themeReducers']);
-
-    const dispatch = useDispatch();
-
-    const switchT = () => {
-        dispatch(switchTheme());
-    };
-
     return (
         <div className={css.main_header}>
             <div className={rootCss.container}>
@@ -42,10 +33,6 @@ const Header = () => {
                     </div>
                     <div className={css.header_links}>
                         <div>
-                            <button onClick={() => switchT()}
-                                    className={dark ? css.dark_switch_btn : css.switch_btn}>
-                                {dark ? 'White Theme' : 'Dark Theme'}
-                            </button>
                         </div>
                         <NavLink className={css.header_link} to={'/'}>Places</NavLink>
                         <NavLink className={css.header_link} to={'/'}>Cooperation</NavLink>
@@ -60,12 +47,6 @@ const Header = () => {
                 </div>
             </div>
             <div className={show ? css.burger_menu_show : css.burger_menu}>
-                <div>
-                    <button onClick={() => switchT()}
-                            className={dark ? css.dark_switch_btn : css.switch_btn}>
-                        {dark ? 'White Theme' : 'Dark Theme'}
-                    </button>
-                </div>
                 <NavLink className={css.header_link} to={'/'}>Login</NavLink>
                 <NavLink className={css.header_link} to={'/'}>Places</NavLink>
                 <NavLink className={css.header_link} to={'/'}>Cooperation</NavLink>
